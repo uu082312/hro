99e42ea2945df518bc2960488d4d6320d9042d6c
lipeishun <lips5748@joyowo.com>
2019-08-02 11:31:34 +0800

    删完了

--- hro/run.py
+++
@@ -1,7 +0,0 @@
-from hro.loggin import loggin
-from hro.read_data import data
-
-
-if __name__ == '__main__':
-    data_list = data()
-    loggin(data_list)
--- hro/tw/run.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import time
-from hro.tw.login import main_fnc
-
-
-if __name__ == '__main__':
-    main_fnc("https://tianwu.joyowo.com/admin/", "luoxy", "Fubin@2613")
-    print("录入完成")
-    time.sleep(10000)
